480febf1ee7c374a30e6375f8450df15f7da1ae5
Toma Siklic <toma.siklic@gmail.com>
2023-02-14 22:29:29 +0100

    starting dropdown menu

--- scripts/script.js
+++ scripts/script.js
@@ -8,7 +8,6 @@
 // })
 
 const inputContainerTexts = document.querySelectorAll('.input-container--text');
-console.log(inputContainerTexts)
 // for (let i = 0; i<inputContainerTexts.length; i++){
 //   inputContainerTexts[i].addEventListener('click', function(e){
 //     console.log(inputContainerTexts[i]);
@@ -18,6 +17,9 @@ console.log(inputContainerTexts)
 
 
 const inputBoxes = document.querySelectorAll('.input-container-box');
+
+inputBoxes.forEach((inputBox) => (inputBox.defaultValue = 'User'))
+
 inputBoxes.forEach((inputBox) => {
   inputBox.addEventListener('mouseover', function(e){
     inputBox.style.backgroundColor = '#1360a011';
@@ -39,7 +41,30 @@ for (let i = 0; i<inputBoxes.length; i++){
 
 for (let i = 0; i<inputBoxes.length; i++){
   inputBoxes[i].addEventListener('blur', function(e){
-    // inputBoxes[i].style.outlineColor = '#1360a0'
     inputContainerTexts[i].style.color = '#414141';
+  })
+}
+
+// const dropdowns = document.querySelectorAll('.dropdown');
+const inputBoxDropdowns = document.querySelectorAll('.input-container-box__dropdown')
+console.log(inputBoxDropdowns)
+
+const dropdownMenus = document.querySelectorAll('.dropdown-menu') 
+
+
+
+for(let i = 0; i<inputBoxDropdowns.length; i++){
+  inputBoxDropdowns[i].addEventListener('focus', function(e){
+    inputBoxDropdowns[i].style.border = '2px solid #1360a0'
+    dropdownMenus[i].classList.add('dropdown-menu__active')
+
+  })
+}
+
+for(let i = 0; i<inputBoxDropdowns.length; i++){
+  inputBoxDropdowns[i].addEventListener('blur', function(e){
+    inputBoxDropdowns[i].style.border = '1px solid black'
+    dropdownMenus[i].classList.remove('dropdown-menu__active')
+
   })
 }
